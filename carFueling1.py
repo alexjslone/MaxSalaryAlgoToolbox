a4e2555e967451223061e9600d0935e6e3132724
Alex Slone <alexjslone12@gmail.com>
2023-02-23 11:21:11 +0700

    added in code to solve problem

--- carFueling1.py
+++ carFueling1.py
@@ -1,7 +1,54 @@
+distance = 200
+tank = 250
+stops = [100, 150]
 def min_refills(distance, tank, stops):
-    fillup= 0
-    for i in stops: 
-        if i > tank: 
-            fillup = 1
+    #gasLeft will be used to track whether we can make it to first stop 
+    #initialize it to the tank because this is as far we can go. Each 
+    #time we fill up we will reset it to the tank 
+    gasLeft = tank
+    numberFills= 0 
+    distance2 = 0 
+    #distance will be part of a while loop until we hit the distance than the
+    #loop should continue
+    count = 1
+    #count variable will be used to check condition of the next gas stop
+    #basically can you make it to the next staion
+    while distance2 < distance:
+        for i in stops: 
+            gasLeft -= i
+            distance2 = i
+            #subtracting the distance 
+            if stops[len(stops)-1]<tank:
+                distance2 = distance
+                numberFills=0
+                break
+            #taking care of condition of tank being enought to get you to 
+            #last stop
+            elif count == len(stops):
+                if gasLeft < distance - distance2: 
+                    numberFills = numberFills + 1
+                    distance2= distance
+                else: 
+                    distance2 = distance
+            elif (stops[count]-i)>tank:
+                distance2=distance
+                numberFills=-1
+                break
+            elif gasLeft == 0: 
+                gasLeft = tank +i 
+                numberFills= numberFills+1  
+            #this condition checks for the last station
+            elif (stops[count]-distance2)>gasLeft and numberFills !=-1:
+                gasLeft= tank + i 
+                numberFills =numberFills + 1 
+            else: 
+                gasLeft = tank
+            count+=1
+    print(numberFills)
+min_refills(distance, tank, stops)
+
         
+
+
+    
 
